8a04ccd63710b039c664d597f470f4e052966455
Ayush Agarwal <thisisayushaa@gmail.com>
2017-11-04 17:11:08 +0530

    Add Initial Template(s)

--- main/views.py
+++ main/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .utils import *
 
 # Create your views here.
+
+def resultsView(request):
+    return render(request, 'main/base.html')
