a5bc0f3634eddc0ea4f766b2d5a4c1b78638f05e
Ayush Agarwal <thisisayushaa@gmail.com>
2017-11-04 16:31:47 +0530

    Add URLS and Accounts App

--- meet/urls.py
+++ meet/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^portal/', include('main.urls', namespace='main')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 ]
